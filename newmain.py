7ab5ce8c3253afb0c0cf4b4917ebac4c2e4a527d
sandeepkumar17212 <sandeepkumar17212@gmail.com>
2021-09-25 03:06:54 +1000

    nothing new

---
+++ newmain.py
@@ -0,0 +1,20 @@
+
+print("from here")
+import users
+
+##import restaurent
+
+print("check from here")
+
+## creating an instance of class user
+
+user1 = users.user("Amit","kumar",20,234332)
+
+
+## calling the methods of the class user as USER1
+user1.gretting()
+user1.describeuser()
+
+
+
+##print(restaurent.grandhayat.number_served)
